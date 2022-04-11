28f0f0fbe8006fb2d4df447c9aae5734196f86a0
Wolfero <ferorock44@gmail.com>
2022-04-11 10:52:17 +0100

    ♻ Refactor

--- src/main/java/WordWrapper.java
+++ src/main/java/WordWrapper.java
@@ -1,9 +1,14 @@
 public class WordWrapper {
-    public String wrap(String text,int columnsWidth){
+    public String wrap(String text, int columnsWidth) {
         if (text.length() == 0) return "";
 
-        if (columnsWidth < 0) throw new IllegalArgumentException("Negative numbers are not allowed");
+        isColumnIsNegative(columnsWidth);
+
 
         return text;
+    }
+
+    private void isColumnIsNegative(int columnsWidth) {
+        if (columnsWidth < 0) throw new IllegalArgumentException("Negative numbers are not allowed");
     }
 }
