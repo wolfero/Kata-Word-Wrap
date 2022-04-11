c4561b3f908a9d877c42447842f4685f8de666ec
Wolfero <ferorock44@gmail.com>
2022-04-11 17:28:51 +0100

    🔴 test: Returns wrapped string on space and does not break the word

--- src/__tests__/WordWrap.test.ts
+++ src/__tests__/WordWrap.test.ts
@@ -24,5 +24,9 @@ describe("Word Wrap Should", () => {
       let wrapper = new Wrapper();
       expect(wrapper.wrap("test", 3)).toBe("tes\nt");
     });
+    it("Returns wrapped string on space and does not break the word", () => {
+      let wrapper = new Wrapper();
+      expect(wrapper.wrap("i'm doing TDD in TypeScript", 6)).toBe("i'm\ndoing\nTDD in\nTypeSc\nript");
+    });
   });
 });
