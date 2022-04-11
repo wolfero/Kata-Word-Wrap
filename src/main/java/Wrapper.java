6c4cdd1a3bd2f7eef471dddcb98140bcb90e882b
Wolfero <ferorock44@gmail.com>
2022-04-11 13:54:45 +0100

    🟢 test: zero_are_not_allowed

--- src/main/java/Wrapper.java
+++ src/main/java/Wrapper.java
@@ -2,6 +2,8 @@ public class Wrapper {
     public String wrap(String text, int columnsWidth) {
         if (text.length() == 0) return "";
 
+        if (columnsWidth == 0) throw new IllegalArgumentException("Zero are not allowed");
+
         isColumnIsNegative(columnsWidth);
 
         if (text.length() <= columnsWidth) return text;
