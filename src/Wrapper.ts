ffc580ad2dd556ea0bc3f6d4fe974f54419e26d0
Wolfero <ferorock44@gmail.com>
2022-04-11 16:44:39 +0100

    ♻ Refactor

--- src/Wrapper.ts
+++ src/Wrapper.ts
@@ -2,9 +2,11 @@ export class Wrapper {
   public wrap(text: string, columnWidth: number): string {
     if (text === "") return "";
 
-    if (columnWidth < 0) {
-      throw "Negative numbers are not allowed";
-    }
+    this.isColumnWidthNegative(columnWidth);
     return text;
+  }
+
+  private isColumnWidthNegative(columnWidth: number) {
+    if (columnWidth < 0) throw "Negative numbers are not allowed";
   }
 }
