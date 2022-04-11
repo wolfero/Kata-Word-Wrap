e2ca420e58200ed9d33e8ffcb1b16cdbe0a9ce05
Wolfero <ferorock44@gmail.com>
2022-04-11 16:40:13 +0100

    🟢 test: Returns an empty string if the text is not received

--- src/Wrapper.ts
+++ src/Wrapper.ts
@@ -1,5 +1,6 @@
-export class Wrapper{
-  public wrap(text:string,columnWidth:number):string{
-    return "-1";
+export class Wrapper {
+  public wrap(text: string, columnWidth: number): string {
+    if (text === "") return "";
+    return text;
   }
 }
--- src/__tests__/WordWrap.test.ts
+++ src/__tests__/WordWrap.test.ts
@@ -1,12 +1,10 @@
 import { Wrapper } from "../Wrapper";
 
 describe("Word Wrap Should", () => {
-
-  describe("Exception handling",()=>{
+  describe("Exception handling", () => {
     it("Returns an empty string if the text is not received", () => {
-      let wrapper=new Wrapper();
-      expect(wrapper.wrap("",4)).toBe("");
+      let wrapper = new Wrapper();
+      expect(wrapper.wrap("", 4)).toBe("");
     });
-
   });
 });
