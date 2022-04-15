b3ba2af4f512159f6f2c3c22d1d97184a7b59510
Wolfero <ferorock44@gmail.com>
2022-04-15 14:56:56 +0100

    ♻ Refactor

--- src/word_wrap.py
+++ src/word_wrap.py
@@ -4,8 +4,14 @@ class WordWrapper:
 
         if len(text) == 0:
             return ""
+        
+        if len(text) <= columnWidth:
+            return text
 
-        return text[0:columnWidth]+"\n"+text[columnWidth]
+        wrappedText=text[0:columnWidth]+"\n"
+        remainingText=text[columnWidth]
+
+        return wrappedText +self.wrap(remainingText,columnWidth)
 
     def isNegativeNumber(self, columnWidth):
         if columnWidth <= 0:
