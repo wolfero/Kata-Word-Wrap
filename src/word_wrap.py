8d20c38f0ad5b6c15de338804708c8e3fc54c664
Wolfero <ferorock44@gmail.com>
2022-04-15 14:14:56 +0100

    ♻ Refactor

--- src/word_wrap.py
+++ src/word_wrap.py
@@ -1,5 +1,12 @@
 class WordWrapper:
     def wrap(self, text: str, columnWidth: int) -> str:
+        self.isNegativeNumber(columnWidth)
+
         if len(text) == 0:
             return ""
-        raise ValueError("Column width must be greater than 0")
+
+        return text
+
+    def isNegativeNumber(self, columnWidth):
+        if columnWidth < 0:
+            raise ValueError("Column width must be greater than 0")
